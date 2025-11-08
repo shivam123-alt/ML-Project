985290827ef6dce1252d8b0e06b75c2e03972dc1
Shivam123-alt <shivam14032004@gmail.com>
2025-11-08 12:46:17 +0530

    Update Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -2,7 +2,6 @@ import os
 import sys
 from dataclasses import dataclass
 
-from sklearn.model_selection import GridSearchCV
 from catboost import CatBoostRegressor
 from sklearn.ensemble import(
     AdaBoostRegressor,
@@ -45,65 +44,54 @@ class ModelTrainer:
                 "Decision Tree":DecisionTreeRegressor(),
                 "Gradient Boosting":GradientBoostingRegressor(),
                 "Linear Regression":LinearRegression(),
-                "K-Neighbors classifier":KNeighborsRegressor(),
-                "XGBClassifier":XGBRegressor(),
-                "CatBoosting Classifier":CatBoostRegressor(),
-                "AdaBoost Classifier":AdaBoostRegressor(),
+                "K-Neighbors Regressor": KNeighborsRegressor(),
+                "XGBRegressor":XGBRegressor(),
+                "CatBoosting Regressor":CatBoostRegressor(verbose=0),
+                "AdaBoost Regressor":AdaBoostRegressor(),
             }
             
             # Hyperparameter grids for tuning each model
-            params = {
-                "Random Forest": {
-                    "n_estimators": [50, 100, 150],
-                    "max_depth": [None, 10, 20]
-                },
+            params={
                 "Decision Tree": {
-                    "max_depth": [None, 10, 20, 30]
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
                 },
-                "Gradient Boosting": {
-                    "n_estimators": [50, 100],
-                    "learning_rate": [0.01, 0.1, 0.2]
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
                 },
-                "Linear Regression": {},  # no major hyperparameters
-                "K-Neighbors": {
-                    "n_neighbors": [3, 5, 7]
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
                 },
-                "XGBoost": {
-                    "n_estimators": [50, 100],
-                    "learning_rate": [0.01, 0.1],
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
                 },
-                "CatBoost": {
-                    "iterations": [50, 100],
-                    "learning_rate": [0.01, 0.1]
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
                 },
-                "AdaBoost": {
-                    "n_estimators": [50, 100],
-                    "learning_rate": [0.01, 0.1]
-                }
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "K-Neighbors Regressor": {},
+                
             }
-            best_models = {}
-            best_scores = {}
-            for name, model in models.items():
-                param_grid = params.get(name, {})
-                if param_grid:
-                    gs = GridSearchCV(model, param_grid, cv=3, scoring="r2", n_jobs=-1, verbose=1)
-                    gs.fit(X_train, y_train)
-                    best_models[name] = gs.best_estimator_
-                    best_scores[name] = gs.best_score_
-                    logging.info(f"Best params for {name}: {gs.best_params_}")
-                else:
-                    model.fit(X_train, y_train)
-                    score = model.score(X_train, y_train)
-                    best_models[name] = model
-                    best_scores[name] = score  # use training score as GridSearch not used
-            
-            # Evaluating model
-            model_report:dict=evaluate_model(X_train=X_train,
-                                             y_train=y_train,
-                                             X_test=X_test,
-                                             y_test=y_test,
-                                             models=models
-                                             )
+
+            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,param=params)
             
             # To get the best model score from dict
             best_model_score = max(sorted(model_report.values()))
