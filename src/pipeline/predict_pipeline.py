6bff6f17ea96b31a1d672b7e332ee04bea200fe5
Shivam123-alt <shivam14032004@gmail.com>
2025-11-10 23:00:05 +0530

    update

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -9,8 +9,8 @@ class PredictPipeline:
     
     def predict(self,features):
         try:
-            model_path = 'artifacts\model.pkl'
-            preprocessor_path='artifacts\preprocessor.pkl'
+            model_path = 'artifacts/model.pkl'
+            preprocessor_path='artifacts/preprocessor.pkl'
             model = load_object(file_path=model_path)
             preprocessor = load_object(file_path=preprocessor_path)
             data_scaled = preprocessor.transform(features)
@@ -26,22 +26,22 @@ class PredictPipeline:
 class CustomData:
     def __init__(self,
                  gender:str,
-                 reace_ethnicity: str,
+                 race_ethnicity: str,
                  parental_level_of_education,
                  lunch:str,
-                 test_prepration_course:str,
+                 test_preparation_course:str,
                  reading_score: int,
                  writing_score: int):
         
         self.gender = gender
         
-        self.reace_ethnicity = reace_ethnicity
+        self.race_ethnicity = race_ethnicity
         
         self.parental_level_of_education=parental_level_of_education
         
         self.lunch=lunch
         
-        self.test_prepration_course=test_prepration_course
+        self.test_preparation_course=test_preparation_course
         
         self.reading_score=reading_score
         
@@ -51,10 +51,10 @@ class CustomData:
         try:
             custom_data_input_dict = {
                 "gender" : [self.gender],
-                "race_ethnicity":[self.reace_ethnicity],
+                "race_ethnicity":[self.race_ethnicity],
                 "parental_level_of_education":[self.parental_level_of_education],
                 "lunch":[self.lunch],
-                "test_prepration_course]":[self.test_prepration_course],
+                "test_preparation_course":[self.test_preparation_course],
                 "reading_score":[self.reading_score],
                 "writing_score":[self.writing_score]
                 
